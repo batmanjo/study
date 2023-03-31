933e6a88b8e4bab92ca4b143a065bf9095843ca5
wuyanmiao <yanmiao.wu@tcl.com>
2023-03-31 10:02:43 +0800

    删除多余文件，上传到git

--- src/main/java/my/study/StudyApplication.java
+++ src/main/java/my/study/StudyApplication.java
@@ -3,6 +3,10 @@ package my.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yanmiao.wu
+ * 启动类
+ */
 @SpringBootApplication
 public class StudyApplication {
 
